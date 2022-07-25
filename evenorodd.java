2eccc5f725dca65cb2b13ea2bc63f9a5b774805f
PriyanshuSha <109303639+PriyanshuSha@users.noreply.github.com>
2022-07-25 16:58:15 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,28 @@
+//findEvenOdd
+
+class evenOrOdd{
+
+public static void main(String args [])
+{
+
+     int num=27;
+   
+   if(num%2==0)//logic behind the code
+   {
+   
+     System.out.println( num + "number is even" );
+	
+   
+   }
+   else
+   {
+   
+     System.out.println( num + "number is odd" );
+	 
+   }
+   
+   
+
+}
+
+}
