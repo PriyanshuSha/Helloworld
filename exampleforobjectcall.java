f8a5fd50ea43a38c6dd90d9550f3de49da743100
PriyanshuSha <109303639+PriyanshuSha@users.noreply.github.com>
2022-07-25 17:52:46 +0530

    Add files via upload

---
+++ exampleforobjectcall.java
@@ -0,0 +1,22 @@
+//exampleForObejct
+
+class exampleForObject
+{
+ 
+ public static void main (String args [])
+ {
+ 
+   System.out.println (" Hello world ");
+   
+   exampleForObject obj=new exampleForObject(); 
+   obj.display();
+ 
+ }
+ public void display()
+ {
+ 
+   System.out.println (" Hello sir ");
+ 
+ }
+
+}
