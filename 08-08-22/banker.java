91603096498b08f219c5184234774c84ee940aa2
PriyanshuSha <109303639+PriyanshuSha@users.noreply.github.com>
2022-08-08 14:46:44 +0530

    Update banker.java

--- 08-08-22/banker.java
+++ 08-08-22/banker.java
@@ -1,28 +1,28 @@
 
-package ps.priyanshu;
+package ps.priyanshu; // it is our package 
 
-import java.util.Scanner;
-import ps.priyanshu.*;
+import java.util.Scanner; // Scanner class
+import ps.priyanshu.*; // here we import our package in which our sub class or customer class is store
 
-class banker
+class banker // This is our main class 
 {
- public static void main (String args [])
+ public static void main (String args []) // this is our main methid  
  {
-     Scanner scan = new Scanner(System.in);
+     Scanner scan = new Scanner(System.in); // it is a Scanner object
 	 System.out.println("Enter account holder name = ");
-	 String userName=scan.next();
+	 String userName=scan.next();  // here we read account holder name from user
 	 System.out.println("Enter your account number = ");
-	 int accountNum = scan.nextInt();
+	 int accountNum = scan.nextInt(); // here we read account number from user 
 	 System.out.println("Enter your account balance = ");
-	 int accountbal = scan.nextInt();
+	 int accountbal = scan.nextInt(); // here we read account balance
 	 System.out.println("Enter amount = ");
-	 int amount = scan.nextInt();
+	 int amount = scan.nextInt();   
  
-     customer user = new customer();
-	 user.creatAccount(userName,accountNum,accountbal);	
-	 user.depositeAmount(amount);
-	 user.withdrawAmount(amount);
-	 user.getDetail();
-	 scan.close();
+     customer user = new customer(); // it is object of our sub class or customer class
+	 user.creatAccount(userName,accountNum,accountbal); // sub class object is use for get deatils from creatAccount method 
+	 user.depositeAmount(amount); // sub class object  is use for get details from depositeAmmount method
+	 user.withdrawAmount(amount); //ub class object  is use for get details from withdrawAmmount method
+	 user.getDetail();  // user object is use for get details from getDetails method
+	 scan.close();  // close scanner object
  }
 }
