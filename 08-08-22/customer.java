d221ebe64c6264d0afa050fe98c91c0bcabc7f1e
PriyanshuSha <109303639+PriyanshuSha@users.noreply.github.com>
2022-08-08 14:53:59 +0530

    Update customer.java

--- 08-08-22/customer.java
+++ 08-08-22/customer.java
@@ -1,34 +1,34 @@
 /*  This is a java program of using packages */
 
-package ps.priyanshu;
+package ps.priyanshu; // it is our package 
 
-class customer
+class customer //  it is our Sub class
 {
    int accountNum,accountbal,amount;
    String userName;
    
-   public  void creatAccount(String name, int num, int bal)
+   public  void creatAccount(String name, int num, int bal) // it is creatAccount method in which user enter details which is neccessary for account creation
    {
     userName=name;
 	accountNum=num;
 	accountbal=bal;
 	}
 	
-	public void withdrawAmount(int ammount)
+	public void withdrawAmount(int ammount) // it is withdrawAmount method for delete balance from ammount
 	{
 	  accountbal-=ammount;
 	  System.out.println(userName+" this is your account balance Rs "+accountbal+" after withdrwaing = Rs "+ammount);
 	  
 	}
 
-    public void depositeAmount(int ammount)
+    public void depositeAmount(int ammount)  // it is a depositeAmount method for add amount in balance
 	{
 	  accountbal+=ammount;
 	  System.out.println(userName+" this is your account balance Rs "+accountbal+" after depositing = Rs "+ammount);
 	  
 	}
   
-     public void getDetail()
+     public void getDetail()  // it is a method for display account holders account details
 	 {
 		System.out.println(" Here is your complete account info");
 		System.out.println("Account holder name is = "+ userName );
